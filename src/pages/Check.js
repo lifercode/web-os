501ceb22a30ee42f71a0e4c46955a86f0105f553
Andre <lifercode@gmail.com>
2023-05-09 14:49:25 -0300

    first fix

--- src/pages/Check.js
+++ src/pages/Check.js
@@ -1,8 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ProgressBar  } from 'react95';
 
 function Check() {
   const [percent, setPercent] = useState(0);
+
+  useEffect(() => {
+    setPercent(0)
+  }, [])
 
   return <ProgressBar value={Math.floor(percent)} />;
 }
